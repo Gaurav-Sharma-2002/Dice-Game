843a7024867a4b283c1993044a8a27532a2c7178
Gaurav Sharma <97343986+Gaurav-Sharma-2002@users.noreply.github.com>
2023-08-29 15:07:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// image 1
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6 + 1;
+randomNumber1 = Math.floor(randomNumber1);
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+// image 2
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+var randomImageSource2 = "images/" + "dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+// heading 
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+
+else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
